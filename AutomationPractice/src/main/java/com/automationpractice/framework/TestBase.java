a08c6f84d60067235502c810219a6f31d85b4000
rkreja <rkrejanyc@yahoo.com>
2019-11-10 15:12:53 -0500

    jenkins update1

--- AutomationPractice/src/main/java/com/automationpractice/framework/TestBase.java
+++ AutomationPractice/src/main/java/com/automationpractice/framework/TestBase.java
@@ -4,16 +4,20 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+@Listeners(ExtentTestNGITestListener.class)
 public class TestBase {
 	
 	 public WebDriver driver;
 	
-	@BeforeSuite
+	@BeforeMethod
 	public void init() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
@@ -23,8 +27,9 @@ public class TestBase {
 	}
 
 
-	
+	@AfterMethod
 	public void closeWindow() {
 		driver.close();
+		ExtentManager.getInstance().flush();
 	}
 }
